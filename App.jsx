00b4c1d36bfa78af13fb8857dc12186e4485e5b3
Alok <134001601+Alokmall@users.noreply.github.com>
2024-02-11 18:56:08 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-// import Findcoder from "./Components/Findcoder"
-// import{ BrowserRouter, Route, Routes} from "react-router-dom"
-// import Explore from './Components/Explore';
-// import Hire from './Components/Hire';
-// import Challenge from './Components/Challenge';
-// import Dev from './Components/Dev';
 import{ BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './CRUD/Home';
 import Createuser from './CRUD/Createuser';
 import Users from './CRUD/Users';
-
 import Editusers from './CRUD/Editusers';
 const App = () => {
     return (
@@ -22,17 +15,6 @@ const App = () => {
             <Route element={<Editusers/>} path="/edit/:abc" />
            </Routes>
            </BrowserRouter>
-
-            {/* <BrowserRouter>
-            <Findcoder/>
-            <Routes>
-                <Route element={<Explore/>}path='/explore'></Route>
-                <Route element={<Hire/>} path='/hire'></Route>
-                <Route element={<Dev/>}  path='/dev'></Route>
-                <Route element={<Challenge/>}  path='/challenge'></Route>
-            </Routes>
-            </BrowserRouter> */}
-            
         </div>
     );
 };
